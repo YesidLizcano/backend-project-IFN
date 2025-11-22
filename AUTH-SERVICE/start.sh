e73904cba3e26e980019753554aba03a941cd6a4
lizcano <blizcano777@gmail.com>
2025-11-21 20:37:57 -0500

    .

--- AUTH-SERVICE/start.sh
+++ AUTH-SERVICE/start.sh
@@ -3,4 +3,4 @@
 # Inicia el servidor FastAPI de autenticación
 # AJUSTAR la ruta si el archivo no es main.py o si está dentro de una carpeta 'src'
 echo "Iniciando Uvicorn para AUTH-SERVICE..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn src.main:app --host 0.0.0.0 --port $PORT
