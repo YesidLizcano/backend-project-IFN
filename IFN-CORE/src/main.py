1062e7dbf187abbbe1d88529c86c43052bcae9e1
lizcano <blizcano777@gmail.com>
2025-11-23 12:41:37 -0500

    feat: Actualizacion URL Frontend

--- IFN-CORE/src/main.py
+++ IFN-CORE/src/main.py
@@ -13,7 +13,10 @@ app = FastAPI(lifespan=create_all_tables)
 # --- Configuración CORS ---
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["https://ifnfrontend-mpm83c2ip-brayan-lizcanos-projects.vercel.app"],
+	allow_origins=[
+        "https://ifnfrontend-mpm83c2ip-brayan-lizcanos-projects.vercel.app",
+        "https://ifnfrontend.vercel.app"
+    ],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
